f78e89cc682be230766f365aa925cfdae8e9a04d
indexofire <indexofire@gmail.com>
2024-04-09 11:28:43 +0800

    version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # enasra
-get sra data from ena
+
+Enasra is a tool to download WGS PE raw data from ENA.
+
+## Usage
+
+**Install**
+
+```shell
+$ mamba install aspera-cli curl
+```
+
+**download**
+
+```shell
+# create list or you can download a list from NCBI
+$ echo -e "DRR123021\nERR1351990" > list
+
+# run pipeline to download
+$ ./getend.sh -i list -d output
+# or you want to run it backend
+$ nohup ./getend.sh -i list -d output &
+
+# check md5sum result
+$ cat output/enasra.log
--- getsra.sh
+++ getsra.sh
@@ -17,15 +17,45 @@ do
     esac
 done
 
-if [ -f "$CONDA_PREFIX/etc/aspera/aspera_bypass_dsa.pem"]; then
+mkdir -p $OUTPUT
+
+if [ -f "$CONDA_PREFIX/etc/aspera/aspera_bypass_dsa.pem" ]; then
     SSH_FILE="$CONDA_PREFIX/etc/aspera/aspera_bypass_dsa.pem"
-elif [ -f "$HOME/.aspera/etc/asperaweb_id_dsa.openssh"]; then
+elif [ -f "$HOME/.aspera/etc/asperaweb_id_dsa.openssh" ]; then
     SSH_FILE="$HOME/.aspera/etc/asperaweb_id_dsa.openssh"
 else
     SSH_FILE=""
     echo "Could not find ascp private key file in system"
     exit 0
 fi
+
+function check_md5 {
+    read md5_1 md5_2 <<< $(curl -G -d 'accession=$1' -d 'result=read_run' -d 'fields=fastq_md5' "https://www.ebi.ac.uk/ena/portal/api/filereport" | cut -f1 | tail -1 | tr ';' ' ')
+    md5file_1=$(md5sum ${OUTPUT}/${1}_1.fastq.gz | cut -f1 -d ' ')
+    md5file_2=$(md5sum ${OUOPUT}/${1}_2.fastq.gz | cut -f1 -d ' ')
+    for i in 1 2
+    do if [[ $md5_${i} == $md5file_${i} ]]; then 
+        echo "$1_${i}.fastq.gz md5 is correct" >> ${OUTPUT}/enasra.log
+    else
+        echo "$1_${i}.fastq.gz md5 is not correct" >> ${OUTPUT}/enasra.log
+    fi
+    done
+}
+
+function get_fastq {
+    if [ ${#1} -gt 10 ]; then
+        nf="0${i:0-2:2}/"
+    elif [ ${#1} -eq 10 ]; then
+        nf="00${1:0-1:1}/"
+    else
+        nf=""
+    fi
+    for pe in 1 2
+    do if [ ! -f "${OUTPUT}/${1}_${pe}.fastq.gz" ]; then
+	ascp -T -l 300m -P33001 -i ${SSH_FILE} era-fasp@fasp.sra.ebi.ac.uk:vol1/fastq/${1:0:6}/${nf}${1}/${1}_${pe}.fastq.gz ${OUTPUT}/${1}_${pe}.fastq.gz
+    fi
+    done
+}
 
 for i in $(cat $INPUT);
 do
@@ -36,22 +66,6 @@ do
     else
         j=""
     fi
-    ascp -T -l 300m -P33001 -i $SSH_FILE era-fasp@fasp.sra.ebi.ac.uk:vol1/fastq/${i:0:6}/${j}${i}/${i}_1.fastq.gz $OUTPUT/${i}_1.fastq.gz
-    ascp -T -l 300m -P33001 -i $SSH_FILE era-fasp@fasp.sra.ebi.ac.uk:vol1/fastq/${i:0:6}/${j}${i}/${i}_2.fastq.gz $OUTPUT/${i}_2.fastq.gz
-    if [ -f $OUTPUT/${i}_1.fastq.gz && $OUTPUT/${i}_2.fastq.gz]; then
-        check_md5 ${i}
-    else "miss paired fastq file"
-    continue
+    get_fastq ${i}
+    check_md5 ${i}
 done
-
-function check_md5 {
-    read md5_1 md5_2 <<< $(curl -G -d 'accession=$1' -d 'result=read_run' -d 'fields=fastq_md5' "https://www.ebi.ac.uk/ena/portal/api/filereport" | cut -f1 | tail -1 | tr ';' ' ')
-    md5file_1=$(md5sum $OUTPUT/${1}_1.fastq.gz | cut -f1 -d ' ')
-    md5file_2=$(md5sum $OUTPUT/${1}_2.fastq.gz | cut -f1 -d ' ')
-    for i in 1 2
-    do if [ $md5_${i} == $md5file_${i} ]; then 
-        echo '$1_${i}.fastq.gz md5 is correct' >> enasra.log
-    else
-        echo '$1_${i}.fastq.gz md5 is not correct' >> enasra.log
-    fi
-}
